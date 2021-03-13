74f51ef6f4ebfce6f4e8354daa81d39640f62c3f
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-03-12 23:18:21 -0700

    Update src.java

--- src.java
+++ src.java
@@ -41,3 +41,10 @@ class Ideone
 
 
 time complexity  O(n+m)
+	
+	
+	
+	Expl:
+/*For a value k, we can mark its left endpoint a in the original array, along with its right endpoint b in the original array. To distinguish between a and b we can store +k for a and -k for b. Now, we technically have stored all information that the m operations represent, into an array. Most importantly, we stored it in O(m) time./*
+
+The next step is to actually find the maximum value based off of our unique representation of the data. We traverse our array from left to right. Whenever we reach a left endpoint a (which we represented with a positive number), we just add that to our sum. Whenever we reach a right endpoint b (which we represented with a negative number), we subtract that from our sum (well, technically we add a negative value). By doing so, the value sum represents the value that array[i] would have if we had applied all m operations to it. The maximum value of sum that we get while traversing the array is the value we return. If this algorithm is still unclear to you, try walking through HackerRank's Testcase 0 with the code above.
